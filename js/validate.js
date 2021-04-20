3560dd783a84b0c4b2115a507819ba3d691a93a9
kbaitubayev <77764770+kbaitubayev@users.noreply.github.com>
2021-04-19 22:14:20 -0400

    Add files via upload

---
+++ js/validate.js
@@ -0,0 +1,17 @@
+function validate(event) {
+  // TODO - write custom validation logic to validate the longitude and latitude
+  // values. The latitude value must be a number between -90 and 90; the
+  // longitude value must be a number between -180 and 180. If either/both are
+  // invalid, show the appropriate error message in the form, and stop the
+  // form from being submitted. If both values are valid, allow the form to be
+  // submitted.
+  console.log(
+    "TODO - validate the longitude, latitude values before submitting"
+  );
+}
+
+// Wait for the window to load, then set up the submit event handler for the form.
+window.onload = function () {
+  const form = document.querySelector("form");
+  form.onsubmit = validate;
+};
